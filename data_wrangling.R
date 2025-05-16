debc965ee05c9765f874194cf182df699188dd61
Eric Anku <ankueric1@gmail.com>
2025-05-16 08:56:18 +0000

    Filter obese patients prior to weight estimations

--- data_wrangling.R
+++ data_wrangling.R
@@ -162,6 +162,10 @@ print(shapiro_p_values)
 export(df, here("data", "muac.csv")) # CSV version
 export(df, here("data", "muac.Rdata")) # Rdata version
 
+# Filter obese patients
+df_obese <- df |>
+  filter(bmi_cat == "Obese")
+
 # Prediction using existing equation
 ## Crandall 
 #----------------------------------------------
@@ -219,9 +223,6 @@ df$kokong <- kokong_weight(df$height)
 
 
 # Crandall prediction for obese patients
-df_obese <- df |>
-  filter(bmi_cat == "Obese")
-
 df_obese$crandall_prediction <- crandall_weight(df_obese$muac, df_obese$height, df_obese$sex)
 
 ## Export dataset
